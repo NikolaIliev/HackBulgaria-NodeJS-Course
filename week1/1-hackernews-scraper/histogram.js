1c0e53629f0a2d0c6789c1be65db243f50ed5d7c
Nikola Iliev <nikola.iliev93@gmail.com>
2014-10-26 16:43:17 +0200

    Adding histogram solution

--- week1/1-hackernews-scraper/histogram.js
+++ week1/1-hackernews-scraper/histogram.js
@@ -1,48 +1,70 @@
 var request = require('request'),
 	express = require('express'),
-    localStorage = require('node-persist'),
     natural = require('natural'),
+    async = require('async'),
     tokenizer = new natural.WordTokenizer(),
-    utils = require("./utils"),
     MongoClient = require('mongodb').MongoClient,
     url = require("./config.json").mongoConnectionUrl,
     app = express(),
     newArticlesCount = 0,
-    articlesDB;
+    db, collection;
 
-utils.initStorage(localStorage);
-histogram = localStorage.getItem("histogram.json");
-histogram.lastId = histogram.lastId || 0;
-histogram.data = histogram.data || {};
-
-function addKeyword(keyword) {
-	MongoClient.connect(url, function(err, db) {
-		if (!err) {
-			console.log("Connected correctly to server");
-		}
-
-		var collection = db.collection("histogram");
-
-		collection.find({keyword: keyword}).toArray(function (err, data) {
-			if (data) {
-
-			} else {
-				collection.insert({
-					keyword: keyword,
-					count: 1
-				}, function (err, result) {
-					console.log(result);
-				});
+function saveLastId(id, callback) {
+	collection.update({_id: "lastId"}, {lastId: id}, {upsert: true}, function (err, result) {
+			if (err) {
+				console.log("Error saving last id");
 			}
-		});
+		}
+	);
+}
 
-		
+function getLastId(callback) {
+	collection.findOne({_id: "lastId"}, function (err, result) {
+		if (err) {
+			console.log("Error getting last id");
+		}
+		if (result) {
+			callback(result.lastId);
+		} else {
+			saveLastId(0);
+			callback(0);
+		}
+	});
+}
+
+function addKeywords(keywords) {
+	async.each(keywords, function (keyword) {
+		collection.update({keyword: keyword},
+		{$inc: {count: 1}},
+		{upsert: true}, function () {}
+		);
+	}, function (err, result) {
+		if (err) {
+			console.log(err);
+		}
+	});
+}
+
+function getRange(start, size, callback) {
+	console.log('start: ' + start + ' size: ' + size);
+
+	collection.find({}, {
+		sort: {count: -1},
+		skip: start,
+		limit: size
+	}).toArray(function (err, data) {
+		if (err) {
+			console.log(err);
+			callback(err, null);
+		} else {
+			callback(null, data);
+		}
 	});
 }
 
 function fetchRecursive(current, remaining) {
 	if (remaining === 0) {
-		return;
+		getMaxItem();
 	}
 	getArticle(current, remaining);
 }
@@ -51,14 +73,11 @@ function getArticle(articleId, remaining) {
 	request("https://hacker-news.firebaseio.com/v0/item/" + articleId + ".json?print=pretty", function (error, response, body) {
 		var article = JSON.parse(body),
 			articleTitle = article.title || "",
-			articleText = article.text || "";
+			articleText = (article.text || "").toLowerCase();
 
-		tokenizer.tokenize(articleTitle + articleText).forEach(function (word) {
-			var currentWord	 = word.toLowerCase();
-			addKeyword(currentWord);
-		});
+		addKeywords(tokenizer.tokenize(articleTitle + articleText));
 
-		histogram.lastId = articleId;
+		saveLastId(articleId);
 
 		console.log('Read article #' + article.id + ". Type: " + article.type);
 		fetchRecursive(articleId + 1, remaining - 1);		
@@ -69,7 +88,10 @@ function getMaxItem() {
 	request('https://hacker-news.firebaseio.com/v0/maxitem.json', function (error, response, body) {
 		if (!error) {
 			console.log("Fetched max item #" + body);
-			fetchRecursive(histogram.lastId + 1 || 1, body);
+			getLastId(function (id) {
+				console.log("GOT LAST ID " + id);
+				fetchRecursive(id + 1, body - id);
+			});
 		} else {
 			console.error(123);
 		}
@@ -86,36 +108,39 @@ app.all("*", function(req, res, next) {
 
 
 app.get("/keywords", function (req, res) {
-	var result = [],
-		from = +req.param("fromPosition"),
-		direction = req.param("direction") || "next";
+	var direction = req.param("direction") || "next",
+		start = +req.param("fromPosition");
 
-	from = (from && !isNaN(from)) ? from : 0;
-	if (direction === "prev" && from < 10) {
-		direction = "next";
-		from = 0;
-	}
+	start = (direction === "next") ? start : start - 20;
+	start = (start >= 0) ? start : 0;	
 
-	for (var key in histogram.data) {
-		result.push({
-			keyword: key,
-			count: histogram.data[key]
-		});
-	}
-
-	result.sort(function (a, b) {
-		return a.count <= b.count ? 1 : -1;
+	getRange(start, 10, function (err, data) {
+		if (err) {
+			res.end(err);
+		} else {
+			res.json(data.map(function (item, index) {
+				item.rank = start + index + 1;
+				return item;
+			}));
+		}
 	});
-
-	result.forEach(function (item, index) {
-		item.rank = index + 1;
-	});
-
-	result = (direction === "next") ? result.slice(from, from + 10) : result.slice(from - 20, from - 10);
-
-	res.json(result);
 });
 
-getMaxItem();
+MongoClient.connect(url, function (err, database) {
+	db = database;
+	collection = database.collection("histogram");
+	getMaxItem();
+});
+
+process.on('SIGINT', function () {
+	console.log('Exiting and closing db connection.');
+	db.close(function (err, result) {
+		if (err) {
+			console.log(err);
+		} else {
+			process.exit();
+		}
+	});
+});
 
 app.listen(3010);
