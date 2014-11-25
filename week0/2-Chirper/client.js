af7e561add0aeab6a02a50697ed5bef056bdf282
Nikola Iliev <nikola.iliev93@gmail.com>
2014-11-25 11:14:08 +0200

    Added Chirper problem solution

--- week0/2-Chirper/client.js
+++ week0/2-Chirper/client.js
@@ -1,33 +1,66 @@
 var http = require("http"),
+	fs = require("fs"),
 	_ = require("underscore"),
 	config = require("./config.json"),
 	ArgumentParser = require('argparse').ArgumentParser,
 	payload = "",
 	parser = new ArgumentParser({}),
 	pathMapping = {
+		"create": "/chirp",
+		"delete": "/chirp",
 		"getall": "/all_chirps",
+		"getself": "/my_chirps",
+		"getusers": "/all_users",
 		"register": "/register"
 	},
 	methodMapping = {
+		"create": "POST",
+		"delete": "DELETE",
 		"getall": "GET",
+		"getself": "POST",
+		"getusers": "GET",
 		"register": "POST"
 	},
 	callbackMapping = {
-		"getall": getallCallback,
+		"create": defaultCallback,
+		"delete": defaultCallback,
+		"getall": defaultCallback,
+		"getself": defaultCallback,
+		"getusers": defaultCallback,
 		"register": registerCallback
 	};
 
+parser.addArgument(['--create'], {
+	constant: "chirp",
+	nargs: "?"
+});
+parser.addArgument(['--delete'], {
+	constant: "chirp",
+	nargs: "?"
+});
 parser.addArgument(["--getall"], {
 	constant: "all_chirps",
+	nargs: "?"
+});
+parser.addArgument(["--getself"], {
+	constant: "my_chirps",
+	nargs: "?"
+});
+parser.addArgument(["--getusers"], {
+	constant: "all_users",
 	nargs: "?"
 });
 parser.addArgument(["--register"], {
 	constant: "register",
 	nargs: "?"
 });
+parser.addArgument(["--chirpid"]);
+parser.addArgument(["--user"]);
+parser.addArgument(["--message"]);
 var args = parser.parseArgs(),
 	argsKeys = _.map(args, function (value, key) { return key; } );
-	argsKeys = _.filter(argsKeys)
+
+argsKeys = _.filter(argsKeys, function (key) {return args[key];});
 console.log(args);
 sendRequest(pathMapping[argsKeys[0]], methodMapping[argsKeys[0]], null, callbackMapping[argsKeys[0]]);
 
@@ -45,19 +78,35 @@ function sendRequest(path, method, data, callback) {
 		res.on("end", function () {
 			callback(payload);
 		});
-	});
+	}),
+	data = {
+		args: args,
+		config: config
+	};
 
-	if (method === "POST") {
-		req.write("pesho");
+	if (method !== "GET") {
+		console.log(JSON.stringify(data));
+		req.end(JSON.stringify(data));
+	} else {
+		req.end();
 	}
-
-	req.end();
 }
 
-function getallCallback(payload) {
-	console.log(payload + "lell");
+function defaultCallback(payload) {
+	console.log(payload);
 }
 
 function registerCallback(data) {
-	var data = json.parse(data);
+	var data = JSON.parse(data);
+
+	config.user = data.user;
+	config.key = data.key;
+
+	fs.writeFile('./config.json', JSON.stringify(config), function (err) {
+		if (err) {
+			throw err;
+		}
+
+		console.log('config: ' + JSON.stringify(config));
+	});
 }
--- week0/2-Chirper/server.js
+++ week0/2-Chirper/server.js
@@ -1,31 +1,128 @@
 var http = require('http'),
+  _ = require('underscore'),
   fs = require('fs'),
-  pandaCounter = 0,
   routing = {
-    "/all_chirps": getAllChirps,
-    "/register": registerUser
+    "/all_chirps GET": getAllChirps,
+    "/all_users GET": getAllUsers,
+    "/chirp POST": createChirp,
+    "/chirp DELETE": deleteChirp,
+    "/my_chirps POST": getMyChirps,
+    "/register POST": registerUser
   },
-  data = {
+  db = {
     users: [],
     chirps: []
   };
 
 http.createServer(function (req, res) {
-  console.log(req);
-  routing[req.url](function (err, data) {
-    if (err) {
-    } else {
-      res.writeHead(200, {'Content-Type': "application/json"});
-      res.write(data);
-      res.end();
-    }
-  });
+  var body = "";
+  if (req.method !== "GET") {
+    req.on('data', function (chunk) {
+      if (chunk) {
+        body += chunk;
+      }
+    });
+    req.on('end', function () {
+      routing[req.url + " " + req.method](JSON.parse(body), function (err, data) {
+        if (err) {
+        } else {
+          res.writeHead(200, {'Content-Type': 'application/json'});
+          res.write(JSON.stringify(data));
+          res.end();
+        }
+      });
+    });
+
+  } else {
+    routing[req.url](function (err, data) {
+      if (err) {
+      } else {
+        res.writeHead(200, {'Content-Type': "application/json"});
+        res.write(JSON.stringify(data));
+        res.end();
+      }
+    });
+  }
 }).listen(8080);
 
-function getAllChirps (callback) {
-  callback(null, JSON.stringify(data.chirps));
+function createChirp (data, callback) {
+  var chirp;
+
+  if (findUser(data.config.user) >= 0) {
+    chirp = {
+      "userId": db.users[findUser(data.config.user)].userId,
+      "chirpId": db.chirps.length,
+      "chirpText": data.args.message
+    };
+
+    db.chirps.push(chirp);
+
+    callback(null, {
+      "chirpId": db.chirps.length - 1
+    });
+  } else {
+    callback("User not found");
+  }
 }
 
-function registerUser () {
-  callback(null, " ???" );
+function deleteChirp(data, callback) {
+  db.chirps.splice(_.indexOf(db.chirps, _.findWhere(db.chirps, { chirpId: data.args.chirpid })));
+  console.log(db.chirps);
+  callback(null, {});
+}
+
+function getAllChirps (callback) {
+  callback(null, db.chirps);
+}
+
+function getAllUsers (callback) {
+  callback(null, db.users);
+}
+
+function getMyChirps(data, callback) {
+  var result;
+
+  if (findUser(data.config.user) >= 0) {
+    result = _.where(db.chirps, { userId: db.users[findUser(data.config.user)].userId});
+
+    callback(null, result);
+  } else {
+    callback("User not found");
+  }
+}
+
+function registerUser (data, callback) {
+  var userIndex = findUser(data.args.user),
+      user;
+  
+  if (userIndex < 0) {
+    user = {
+      "user": data.args.user,
+      "key": "" + db.users.length,
+      "userId": db.users.length,
+      "chirps": 0
+    };
+    db.users.push(user);
+  } else {
+    user = db.users[userIndex];
+  }
+
+  console.log(user);
+
+  callback(null, {
+    user: user.user,
+    key: user.key
+  });
+}
+
+function findUser(username) {
+  var i;
+
+  for (i = 0; i < db.users.length; i++) {
+    if (db.users[i].user === username) {
+      return i;
+    }
+  }
+
+  return -1;
 }
